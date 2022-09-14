eb629ca8b407ca8947fe418900b774f5c5335063
aaditdim <aaditdim@usc.edu>
2022-09-14 02:27:31 -0700

    Added the portals and the teleportation feature

---
+++ Game-Prototype/Assets/Scripts/Teleportation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+    public GameObject portal;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+
+            player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
+        }
+    }
+}
