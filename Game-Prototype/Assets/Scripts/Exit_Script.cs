7e52729f1533961320b50d1030eee80dc344ea95
hariniash <94489038+hariniash@users.noreply.github.com>
2022-09-18 14:38:02 -0700

    Update with Analytic manager base

--- Game-Prototype/Assets/Scripts/Exit_Script.cs
+++ Game-Prototype/Assets/Scripts/Exit_Script.cs
@@ -9,10 +9,21 @@ public class Exit_Script : MonoBehaviour
     public GameObject Exit_UI; // UI to display when player arrive the exit
     public TextMeshProUGUI GameFinishText; // Text box to display the text when the game reaches to an end
 
+    public GameObject analyticsManager;
+    private AnalyticsManager analyticsManagerScript;
+    public int kills;
+
     // Start is called before the first frame update
+    private void Awake(){
+        analyticsManagerScript = analyticsManager.GetComponent<AnalyticsManager>();
+    //analyticsManagerScript = analyticsManager.GetComponent<AnalyticsManager>();
+    }
+    
     void Start()
     {
+        //analyticsManagerScript = analyticsManager.GetComponent<AnalyticsManager>();
         Exit_UI.SetActive(false); // Disable(Hide) the UI at the start of the game
+        kills = 5;
     }
 
     // Update is called once per frame
@@ -23,7 +34,12 @@ public class Exit_Script : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider)
     {
+        kills++;
         Exit_UI.SetActive(true); // Enable the UI when detects the collision between player and exit
+        analyticsManagerScript.HandleEvent("kills", new List<object>
+                    {
+                        kills
+                    });
         GameFinishText.text = "Level Passed!";
         Time.timeScale = 0; // Freeze the game (Set value to 1 to continue time flow)
     }
