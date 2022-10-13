7037dcc516f7a7f20fbe15eb6ee6e3b0f3daba48
aaditdim <aaditdim@usc.edu>
2022-10-12 23:02:41 -0700

    Updated Highscore UI

--- Game-Prototype/Assets/Scripts/GameHighscore.cs
+++ Game-Prototype/Assets/Scripts/GameHighscore.cs
@@ -1,9 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameHighscore : MonoBehaviour
 {
+    public TMP_Text pos1;
+    public TMP_Text pos2;
+    public TMP_Text pos3;
+    public TMP_Text pos4;
+    public TMP_Text pos5;
+    public TMP_Text dot1;
+    public TMP_Text dot2;
+    public TMP_Text dot3;
+    public TMP_Text posn1;
+    public TMP_Text posn2;
+    public TMP_Text posn3;
+
+    public TMP_Text score1;
+    public TMP_Text score2;
+    public TMP_Text score3;
+    public TMP_Text score4;
+    public TMP_Text score5;
+    public TMP_Text scoren1;
+    public TMP_Text scoren2;
+    public TMP_Text scoren3;
+
+    public TMP_Text name1;
+    public TMP_Text name2;
+    public TMP_Text name3;
+    public TMP_Text name4;
+    public TMP_Text name5;
+    public TMP_Text namen1;
+    public TMP_Text namen2;
+    public TMP_Text namen3;
+
     // Start is called before the first frame update
     void Start()
     {
