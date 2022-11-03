daff091296c533c512b78b48d2fb2e10b05f3491
George Joseph <thegeorgejoseph@gmail.com>
2022-11-02 20:28:53 -0700

    Adding Conversations in Tutorial Level

---
+++ Game-Prototype/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{   
+    public GameObject[] popUps;
+    private int popUpIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUpIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (popUpIndex < popUps.Length){
+            
+            if (popUpIndex == 0) {
+            popUps[0].SetActive(true);
+            popUps[1].SetActive(false);
+            popUps[2].SetActive(false);
+            popUps[3].SetActive(false);
+            popUps[4].SetActive(false);
+        }
+        else if (popUpIndex == 1) {
+            popUps[1].SetActive(true);
+            popUps[0].SetActive(false);
+            popUps[2].SetActive(false);
+            popUps[3].SetActive(false);
+            popUps[4].SetActive(false);
+        } 
+        else if (popUpIndex == 2) {
+            popUps[2].SetActive(true);
+            popUps[0].SetActive(false);
+            popUps[1].SetActive(false);
+            popUps[3].SetActive(false);
+            popUps[4].SetActive(false);
+        }
+        else if (popUpIndex == 3) {
+            popUps[3].SetActive(true);
+            popUps[0].SetActive(false);
+            popUps[2].SetActive(false);
+            popUps[1].SetActive(false);
+            popUps[4].SetActive(false);
+        }
+        else if (popUpIndex == 4) {
+            popUps[4].SetActive(true);
+            popUps[0].SetActive(false);
+            popUps[2].SetActive(false);
+            popUps[3].SetActive(false);
+            popUps[1].SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+                popUpIndex++;
+            }
+
+        }
+        
+        
+    }
+}
